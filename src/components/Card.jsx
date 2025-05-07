cff739ac988696d80e3fec5b2d216cc3e4afd694
ANILCHINTAPALLY <anilchintapallyrao@gmail.com>
2025-05-07 02:01:23 +0000

    added files

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,15 @@
+// src/components/Card.js
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Card = ({description, alt_description, id, _id, user, urls, likes}) => {
-
-  const style = {
-    backgroundImage: `url(${urls.small})`
-  }
-  
+const Card = ({ product }) => {
   return (
-    <div className="fl w-50 w-25-m w-20-l pa2">
-      <Link to={`/product/${_id}`} className="db link dim tc"> 
-        <div style={style} alt="" class="w-100 db outline black-10 h4 cover"></div>
-        <dl className="mt2 f6 lh-copy">
-          <dt className="clip">Title</dt>
-          <dd className="ml0 black truncate w-100">{description ?? alt_description}</dd>
-          <dt className="clip">Artist</dt>
-          <dd className="ml0 gray truncate w-100">{user.first_name} {user.last_name}</dd>
-          <dt className="clip">Likes</dt>
-          <dd className="ml0 gray truncate w-100">{likes} Likes</dd>
-        </dl>
-      </Link>
+    <div>
+      <h3>{product.name}</h3>
+      <p>Price: ${product.price}</p>
+      <Link to={`/product/${product.id}`}>View Details</Link>
     </div>
-  )
-}
+  );
+};
 
 export default Card;
--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 // TODO: Replace this with your own Server URL for your codespace
 export const BASE_URL = "https://glowing-space-memory-q7p4jvr9g4p424rj7-3080.app.github.dev";
+// src/config.js
+export const BASE_URL = "http://localhost:<PORT_NUMBER>"; // Replace <PORT_NUMBER> with your actual port number
